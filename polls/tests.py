a4367e0921780e0b9bff3efea340839dd5047eb5
Talyson Moreira <talyson.penha@gmail.com>
2021-06-22 21:19:27 -0300

    Question model automated tests updated

--- polls/tests.py
+++ polls/tests.py
@@ -5,10 +5,22 @@ from django.utils import timezone
 
 from .models import Question
 
-#was_published_recently deve retornar Falso para perguntas não recentes
+"""was_published_recently() retorna Falso para perguntas com pub_date futuros"""
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+"""was_published_recently() retorna Falso para perguntas com pub_date de mais de um dia"""
+def test_was_published_recently_with_old_question(self):
+    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+    old_question = Question(pub_date=time)
+    self.assertIs(old_question.was_published_recently(), False)
+
+"""was_published_recently() retorna True para perguntas com pub_date no dia"""
+def test_was_published_recently_with_recent_question(self):
+    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+    recent_question = Question(pub_date=time)
+    self.assertIs(recent_question.was_published_recently(), True)
